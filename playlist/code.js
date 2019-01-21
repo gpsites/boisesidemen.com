50b9b99f4c39e9925863f51c87e1b60bf61819fe
Greg Perkins <greg@collateralcode.com>
2019-01-21 13:51:07 -0700

    fix scrolling inertia; cache data for faster presentation

--- playlist/code.js
+++ playlist/code.js
@@ -1,4 +1,7 @@
 
+const sheetUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vThlre8GwnVTDD1LMt2yfpqDFSnOMaLtYZT-oGc33sy9hpVs5QhA413wTnKXoYQ-JbwAg7Af2Z8rH_6/pub?gid=5&single=true&output=csv";
+const antiCors = "https://sidemen-cors-anywhere.herokuapp.com/";
+
 const normalize = str => str.toLowerCase().replace(/\s+/, ' ');
 
 Vue.use(VueMaterial.default);
@@ -8,7 +11,8 @@ var app = new Vue({
     textFilter: '',
     checkedAttrs: [],
     attrNames: [],
-    tunes: []
+    tunes: [],
+    message: null,
   },
   computed: {
     filtered: function () {
@@ -31,29 +35,38 @@ var app = new Vue({
         this.tunes.splice(Math.floor(Math.random() * last - 1), 0, temp[0]);
       }
     }
-  }
-});
-
-const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vThlre8GwnVTDD1LMt2yfpqDFSnOMaLtYZT-oGc33sy9hpVs5QhA413wTnKXoYQ-JbwAg7Af2Z8rH_6/pub?gid=5&single=true&output=csv";
-const antiCors = "https://sidemen-cors-anywhere.herokuapp.com/";
-
-Papa.parse(antiCors + url, {
-  download: true,
-  complete: function (results) {
-    for (attrName of results.data.shift().slice(2)) {
-      app.attrNames.push(attrName);
-    }
-    for (tune of results.data) {
-      const name = tune.shift();
-      const notes = tune.shift();
-      let attrs = tune.reduce((r, v, i) => v === 'TRUE' ? [...r, app.attrNames[i]] : r, []);
-      app.tunes.push({
-        name,
-        notes,
-        attrs,
-        url: 'files/' + name,
-        search: normalize(name + ' ' + notes)
+  },
+  mounted: function () {
+    this.$nextTick(function () {
+      const cachedData = localStorage.getItem('cachedData'); 
+      if (cachedData) loadRaw(JSON.parse(cachedData));
+    });
+    this.$nextTick(function () {
+      Papa.parse(antiCors + sheetUrl, {
+        download: true,
+        error: (e) => { app.message = 'Error Loading Data: ' + e; },
+        complete: results => loadRaw(results.data)
       });
-    }
+    });
   }
 });
+
+function loadRaw(data) {
+  localStorage.setItem('cachedData', JSON.stringify(data));
+
+  app.attrNames = data.shift().slice(2);
+
+  app.tunes = [];
+  for (tune of data) {
+    const name = tune.shift();
+    const notes = tune.shift();
+    let attrs = tune.reduce((r, v, i) => v === 'TRUE' ? [...r, app.attrNames[i]] : r, []);
+    app.tunes.push({
+      name,
+      notes,
+      attrs,
+      url: 'files/' + name,
+      search: normalize(name + ' ' + notes)
+    });
+  }
+}
